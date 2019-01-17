8b515ce397f796ae500c99a0186755d252bf535a
Marco Stevanon <marco.stevanon@gmail.com>
2019-01-18 00:38:23 +0100

    clean up code on routing files

--- api/route/vote.router.js
+++ api/route/vote.router.js
@@ -1,52 +1,37 @@
 let router = require('express').Router();
 const verifyToken = require('./auth');
+
 const { Client } = require("pg");
-const pg = require('../config/pg.config')
+const pg_options = require('../config/pg.config');
+const updateRedisRanking = require('../worker/updateRedisRanking');
 
-const redis = require('redis');
-const redis_options = require('../config/redis.config')
-
+// /vote
 router.post('/', verifyToken, async (req, res) => {
-    console.log(req.body);
     var args = [req.body.image_id, req.body.user_id, req.body.vote_value];
-    const client = new Client(pg.pg);
 
-    await client.connect();
-    await client.query('BEGIN;');
-    let rows = await client.query(`INSERT INTO tsac18_stevanon.votes (id_image, id_user, value) 
-                                    VALUES ($1, $2, $3)`, args);
+    const pg_client = new Client(pg_options);
+    await pg_client.connect();
+    await pg_client.query('BEGIN;');
 
-    await client.query(`UPDATE tsac18_stevanon.images
-                        SET avg_votes = (SELECT AVG(votes.value)
-                                        FROM tsac18_stevanon.votes
-                                        WHERE votes.id_image = $1),
-                            n_votes = (SELECT COUNT(votes.*)
-                                        FROM tsac18_stevanon.votes
-                                        WHERE votes.id_image = $1)
-                        WHERE id = $1`, [req.body.image_id]);
+    var query = `INSERT INTO tsac18_stevanon.votes (id_image, id_user, value) VALUES ($1, $2, $3)`;
+    await pg_client.query(query, args);
 
-    var rds_client = redis.createClient(redis_options);
-    rds_client.on("error", (err) => console.log("Error " + err));
-    let rows_tocache = await client.query(`
-                    SELECT DISTINCT images.id, users.username as "user", images.url, images.n_votes, images.avg_votes,
-                                    ($1 * images.avg_votes) + ($2 * images.n_votes) as score
-                    FROM tsac18_stevanon.images
-                        JOIN tsac18_stevanon.users ON images.id_user = users.id
-                        LEFT JOIN tsac18_stevanon.votes ON images.id = votes.id_image
-                    WHERE n_votes > 0
-                    ORDER BY score DESC
-                    LIMIT 3;`, [3, 1]);
-
-    rds_client.set("rating", JSON.stringify(rows_tocache.rows), args => {
-        console.log(args);
-        rds_client.quit();
-    });
-
-    await client.query('COMMIT;');
-    await client.end();
-
-    if (rows) res.json(rows.rows);
+    var update_result = await updateRedisRanking();
+    if (update_result) res.status(200).json(update_result);
     else res.sendStatus(500);
+    console.log(update_result);
+
+    query = `UPDATE tsac18_stevanon.images
+                    SET avg_votes = (SELECT AVG(votes.value)
+                                    FROM tsac18_stevanon.votes
+                                    WHERE votes.id_image = $1),
+                        n_votes = (SELECT COUNT(votes.*)
+                                    FROM tsac18_stevanon.votes
+                                    WHERE votes.id_image = $1)
+                    WHERE id = $1`;
+    await pg_client.query(query, [req.body.image_id]);
+    await pg_client.query('COMMIT;');
+    await pg_client.end();
 });
 
 module.exports = router;
