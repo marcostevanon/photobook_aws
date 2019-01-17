c6742ca53bcca5ba0d7a0ea1655268cb23cbff97
Marco Stevanon <marco.stevanon@gmail.com>
2019-01-18 00:17:02 +0100

    introduce rabbitmq

--- api/route/upload.router.js
+++ api/route/upload.router.js
@@ -1,41 +1,66 @@
 let router = require('express').Router();
-const { Client } = require("pg");
 const verifyToken = require('./auth');
+const uuid = require('uuid/v1');
 
 const multer = require('multer');
 var upload = multer({ storage: multer.memoryStorage() });
-
 const s3 = require('../config/s3.config');
 
+const { Client } = require("pg");
+const pg_options = require('../config/pg.config');
+
+const mq = require('amqplib/callback_api')
+const mq_options = require('../config/mq.config.js');
+
+// /upload
 router.post('/', verifyToken, upload.single("image"), (req, res) => {
 
-    const s3Client = s3.s3Client;
-    const params = s3.uploadParams;
+    if (req.file.mimetype.split('/')[0] != 'image')
+        return res.status(400).json({ error: { status: 400, message: "Error -> File type not supported" } });
 
-    params.Key = 'images/' + req.file.originalname;
+    const params = s3.uploadParams;
+    params.Key = `images/raw/${uuid()}.${req.file.originalname}`;
     params.Body = req.file.buffer;
 
-    if (req.file.mimetype.split('/')[0] != 'image')
-        return res.status(400).json({ error: "Error -> File type not supported" });
+    s3.s3Client.upload(params, async (err, data) => {
+        if (err)
+            return res.status(500).json({ error: { status: 500, message: "Error -> " + err } });
 
-    s3Client.upload(params, async (err, data) => {
-        if (err) return res.status(500).json({ error: "Error -> " + err });
-        var msg = { message: 'File uploaded successfully! -> keyname = ' + req.file.originalname };
-        console.log(msg.message);
+        var imageUrl = createCloudfrontURL(data);
 
-        var data = data.Location.split('/');
-        data[2] = 'd3v4yqdep5qmmr.cloudfront.net';
-        var imageUrl = data.join('/');
-        msg.url = imageUrl;
-        console.log(imageUrl);
+        var args = [req.token.id, imageUrl, req.file.originalname];
+        args.push(req.body.title ? req.body.title : null);
+        args.push(req.body.description ? req.body.description : null);
+        
+        console.log(args);
 
-        const client = new Client(require('../config/pg.config').pg);
-        await client.connect();
-        await client.query(`INSERT INTO "tsac18_stevanon".images (id_user, url) VALUES ($1, $2)`, [req.token.id, imageUrl]);
-        await client.end();
+        const pg_client = new Client(pg_options);
+        await pg_client.connect();
+        const query = `INSERT INTO "tsac18_stevanon".images (id_user, raw_image_url, original_name, title, description) VALUES ($1, $2, $3, $4, $5)`;
+        await pg_client.query(query, args);
+        await pg_client.end();
 
+        var msg = { url: imageUrl, author_id: req.token.id }
         res.json(msg);
+
+        mq.connect(mq_options, (err, conn) => {
+            if (err) console.log(err);
+
+            conn.createChannel((err, ch) => {
+                var queue = 'photo_processing';
+
+                ch.assertQueue(queue, { durable: true });
+                ch.sendToQueue(queue, new Buffer.from(JSON.stringify(msg)));
+                setTimeout(() => conn.close(), 100);
+            });
+        });
     });
 });
+
+function createCloudfrontURL(data) {
+    var url = data.Location.split('/');
+    url[2] = s3.cloudfrontURL;
+    return url.join('/');
+}
 
 module.exports = router;
