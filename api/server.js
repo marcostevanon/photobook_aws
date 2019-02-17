978df1aa1a7029a389b76132d52ccdd558fb9468
Marco Stevanon <marco.stevanon@gmail.com>
2019-02-17 01:11:44 +0100

    update api path reference

--- api/server.js
+++ api/server.js
@@ -6,23 +6,18 @@ console.log('Inizializing...');
 
 app.use(cors());
 app.use(body_parser.json());
-app.use('/auth', require('./route/auth.router'))
-app.use('/gallery', require('./route/gallery.router'));
-app.use('/upload', require('./route/upload.router'));
-app.use('/vote', require('./route/vote.router'))
-app.use('/profile', require('./route/profile.router'))
 
-// app.use('/api/auth', require('./route/auth.router'))
-// app.use('/api/gallery', require('./route/gallery.router'));
-// app.use('/api/upload', require('./route/upload.router'));
-// app.use('/api/vote', require('./route/vote.router'))
-// app.use('/api/profile', require('./route/profile.router'))
+app.use('/api/auth', require('./route/auth.router'))
+app.use('/api/gallery', require('./route/gallery.router'));
+app.use('/api/upload', require('./route/upload.router'));
+app.use('/api/vote', require('./route/vote.router'))
+app.use('/api/profile', require('./route/profile.router'))
 
 setTimeout(() => {
-    require('./worker').generateRatingList()
-        .then(() => {
-            const server = app.listen(5671, () => {
-                console.log(`\nApp listening at http://${server.address().address}:${server.address().port}`);
-            });
-        }).catch(err => console.log(err));
+	require('./worker').generateRatingList()
+		.then(() => {
+			const server = app.listen(5671, () => {
+				console.log(`\nApp listening at http://${server.address().address}:${server.address().port}`);
+			});
+		}).catch(err => console.log(err));
 }, 1/*10000*/);
