7c87dbf4d5b6a81951042259089583dad2fc4fea
Marco Stevanon <marco.stevanon@gmail.com>
2019-02-17 01:13:15 +0100

    fix query and params that will be cached

--- api/worker.js
+++ api/worker.js
@@ -1,8 +1,8 @@
 const { Client } = require("pg");
-const pg_options = require('./config/pg.config');
+const pg_options = require('../config/pg.config');
 
 const redis = require('redis');
-const redis_options = require('./config/redis.config');
+const redis_options = require('../config/redis.config');
 
 module.exports.generateRatingList = () => {
     console.log('Recreating redis cache...');
@@ -11,28 +11,38 @@ module.exports.generateRatingList = () => {
     return new Promise(async (resolve, reject) => {
         const pg_client = new Client(pg_options);
 
-        //Select first 5 photos with higer calculated score [score = (1 * image_average_value) + (3 * number_of_votes)]
-        const query = `SELECT DISTINCT images.id, users.username as "user", images.resized_image_url as url, images.n_votes, images.avg_votes,
-                            ($1 * images.avg_votes) + ($2 * images.n_votes) as score
+        //Select first 10 photos with higer calculated score [score = (5 * image_average_value) + (3 * number_of_votes)]
+        const query = `SELECT DISTINCT images.id,
+                                users.username                                as author_username,
+                                users.avatar                                  as author_avatar_url,
+                                images.raw_image_url                          as raw_image_url,
+                                images.resized_image_url                      as thumbnail_url,
+                                images.title                                  as title,
+                                images.description                            as description,
+                                images.tags::TEXT                             as tags,
+                                images.n_votes                                as votes_n,
+                                images.avg_votes                              as votes_avg,
+                                images.timestamp                              as timestamp,
+                                ($1 * images.avg_votes) + ($2 * images.n_votes) as score
                         FROM tsac18_stevanon.images
-                        JOIN tsac18_stevanon.users ON images.id_user = users.id
-                        LEFT JOIN tsac18_stevanon.votes ON images.id = votes.id_image
+                            JOIN tsac18_stevanon.users ON tsac18_stevanon.images.id_user = tsac18_stevanon.users.id
+                            LEFT JOIN tsac18_stevanon.votes ON tsac18_stevanon.images.id = tsac18_stevanon.votes.id_image
                         WHERE n_votes > 0
-                        ORDER BY score DESC
-                        LIMIT 5;`;
+                        ORDER BY score DESC, images.timestamp DESC
+                        LIMIT 10;`;
 
         pg_client.connect()
             .then(() => {
-                return pg_client.query(query, [1, 3]);
-                // 1 = weigth for the number of votes for every image
-                // 3 = weigth for vote average for every image
+                return pg_client.query(query, [5, 3]);
+                // 3 = weigth for the number of votes for every image
+                // 5 = weigth for vote average for every image
             }).then(table => {
                 pg_client.end();
 
                 // updating redis
                 var redis_client = redis.createClient(redis_options);
                 redis_client.on("error", err => reject(err));
-                redis_client.set("rating", JSON.stringify(table.rows), args => {
+                redis_client.set("ranking", JSON.stringify(table.rows), args => {
                     redis_client.quit();
                     console.timeEnd('...done');
 
