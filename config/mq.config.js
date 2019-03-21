5b26fadba3d5dd41a159967bcff1973048674c0d
Marco Stevanon <marco.stevanon@gmail.com>
2019-03-21 11:54:23 +0100

    stuff

--- config/mq.config.js
+++ config/mq.config.js
@@ -1,1 +1,6 @@
-module.exports = { rabbitMQconnectionstring: process.env.RMQ_HOSTNAME }
+module.exports = {
+    rabbitMQconnectionstring:
+        process.env.NODE_ENV == 'prod'
+            ? process.env.RMQ_HOSTNAME_PROD
+            : process.env.RMQ_HOSTNAME_STAGE
+}
