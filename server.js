b0291a08a4a6bbd5e24824912096d056c351ce57
Marco Stevanon <marco.stevanon@gmail.com>
2019-03-21 10:16:13 +0100

    stuff

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
+console.log('Inizializing...');
+console.log(new Date().toUTCString())
+
+// Import environments
 require('dotenv').config();
 
 const app = require('express')();
@@ -6,11 +10,10 @@ const morgan = require('morgan');
 const body_parser = require('body-parser');
 const cron = require('node-cron');
 
-console.log('Inizializing...');
-console.log(new Date().toUTCString())
-
+// Inizializing Pg Pool
 require('./config/pg.config').initPgPool();
 
+// Inizializing Express Api
 app.use(cors());
 app.use(morgan(':date[iso] [:response-time[digits]ms] :remote-addr :method :url :status \t :referrer'));
 app.use(body_parser.json());
@@ -23,14 +26,6 @@ app.use('/api/profile', require('./route/profile.router'));
 app.use('/api/search', require('./route/search.router'));
 app.get('/', (req, res) => res.send('<h1>It works!</h1>'));
 app.all('*', (req, res) => res.sendStatus(404));
-
-function regenerateCache() {
-	require('./workers/redis-worker').generateRatingList()
-		.then(() => require('./workers/elastic-search.worker').updateImagesIndeces())
-		.then(() => require('./workers/elastic-search.worker').updateUsersIndeces())
-		.then(() => require('./workers/rabbit-mq.worker').startListening())
-		.catch(err => console.warn(err));
-}
 
 console.log('Waiting for services start...');
 setTimeout(() => {
@@ -48,3 +43,12 @@ setTimeout(() => {
 }, 45000);
 
 
+
+
+function regenerateCache() {
+	require('./workers/redis-worker').generateRatingList()
+		.then(() => require('./workers/elastic-search.worker').updateImagesIndeces())
+		.then(() => require('./workers/elastic-search.worker').updateUsersIndeces())
+		.then(() => require('./workers/rabbit-mq.worker').startListening())
+		.catch(err => console.warn(err));
+}
