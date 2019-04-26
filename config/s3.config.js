0537c634c11f5fa6689306063c2b5c4fcfeab783
Marco Stevanon <marco.stevanon@gmail.com>
2019-04-26 19:15:32 +0200

    update variable configuration

--- config/s3.config.js
+++ config/s3.config.js
@@ -2,8 +2,8 @@ const AWS = require('aws-sdk');
 
 module.exports = {
     s3Client: new AWS.S3({
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET,
         region: process.env.REGION
     }),
     uploadParams: {
